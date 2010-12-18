32d32c739502febc2f696ffd76237fbd7bae4b08
Marco Fontani <MFONTANI@cpan.org>
2010-12-18 18:23:06 +0000

    Av4::Server uses XSAccessor

--- lib/Av4/Server.pm
+++ lib/Av4/Server.pm
@@ -1,28 +1,25 @@
 package Av4::Server;
-use Moose;
 use Av4::Help;
 use Av4::User;
 
-has 'helps' => (
-    is       => 'rw',
-    isa      => 'ArrayRef[Av4::Help]',
-    required => 1,
-    default  => sub { [] }
-);
-has 'inbuffer'  => ( is => 'rw', isa => 'HashRef', required => 1, default => sub { {} } );
-has 'outbuffer' => ( is => 'rw', isa => 'HashRef', required => 1, default => sub { {} } );
-has 'clients'   => (
-    is       => 'rw',
-    #isa      => 'ArrayRef[Av4::User]', # makes testcover die
-    isa      => 'ArrayRef', # doesn't
-    required => 1,
-    default  => sub { [] }
-);
+use Class::XSAccessor {
+    constructor => '_new',
+    accessors => [ qw/helps inbuffer outbuffer clients kernel running/ ],
+};
 
-has 'kernel' => ( is => 'rw', isa => 'Any', required => 1 );
+sub new {
+    my $class = shift;
+    $class->_new(
+        # defaults
+        helps => [],
+        inbuffer => {},
+        outbuffer => {},
+        clients => [],
+        running => 1,
+        kernel => undef,
+        # wanted options
+        @_,
+    );
+}
 
-has 'running' => ( is => 'rw', isa => 'Int', required => 1, default => 1 );
-
-no Moose;
-__PACKAGE__->meta->make_immutable();
 1;
